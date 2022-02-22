8a96e67e764ee3330e4ad7c37e4cf8f43bd9a7da
Lucia Fernandes <mariapereira820@hotmail.com>
2022-02-22 17:51:14 +0000

    test

--- cypress/integration/firstTest.specs.js
+++ cypress/integration/firstTest.specs.js
@@ -4,5 +4,26 @@ describe('Our first suite', () => {
 
     it('first test', () => {
 
+        //by Tag Name
+        cy.get('input')
+
+        //by ID
+        cy.get('#inputEmail1')
+
+        //by Class name
+        cy.get('.input-full-width')
+
+        //by Attribute name
+        cy.get('[placeholder]')
+
+        //by Attribute name and value
+        cy.get('[placeholder="Email"]')
+
+        //by Class value
+        cy.get('[class="input-full-width size-medium shape-rectangle"]')
+
+        //by Tag name and Attibute
+        cy.get('input[placeholder="Email"]')
+
     })
 })
