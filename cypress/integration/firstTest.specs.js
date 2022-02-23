73ec2d4d8bffca833a0edc157194a3d61cf41d23
Lucia Fernandes <mariapereira820@hotmail.com>
2022-02-23 17:34:20 +0000

    second test buttons

--- cypress/integration/firstTest.specs.js
+++ cypress/integration/firstTest.specs.js
@@ -46,6 +46,18 @@ describe('Our first suite', () => {
         cy.contains('Forms').click()
         cy.contains('Form Layouts').click()
 
+        cy.get('[data-cy="signInButton"]')
+        cy.contains('Sign in')
+        cy.contains('[status="warning"]', 'Sign in')
+        cy.get('#inputEmail3')
+        .parents('form')
+        .find('button')
+        .should('contain', 'Sign in')
+        .parents('form')
+        .find('nb-checkbox')
+        .click
+       
+
     })
 
 })
